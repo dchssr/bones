e4d11a9931fe30091cb8c601e11f118fd5dc28ee
Dave Chesser <me@dchesser.website>
2022-11-08 10:56:49 -0500

    Add "Die" class for an individual die
    At minimum, a Die must have AT LEAST two sides: something equivalent
    to a coin.
    Guess I have a thing called "testing" I'll need to learn to be able to
    replicate results later in die rolls and statistics.

---
+++ src/die.cr
@@ -0,0 +1,30 @@
+module Bones
+  struct Die
+    getter sides : UInt32
+    property randomness : Random = Random::DEFAULT
+
+    def initialize(@sides : UInt32)
+      if (@sides < 2)
+        raise ArgumentError.new("A Die must have at least two sides")
+      end
+    end
+
+    def roll : Int
+      return @randomness.rand(@sides).succ
+    end
+
+    def to_s
+      return "1d#{@sides}"
+    end
+  end
+
+  module Dice
+    D4   = Die.new(4)
+    D6   = Die.new(6)
+    D8   = Die.new(8)
+    D10  = Die.new(10)
+    D12  = Die.new(12)
+    D20  = Die.new(20)
+    D100 = Die.new(100)
+  end
+end
